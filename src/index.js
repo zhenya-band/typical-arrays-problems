f2789b31fa265ae1e476e8e154fe52b34a3234e8
Yauheni Bandyukevich <zhenya.bandyukevich@mail.ru>
2020-10-01 15:43:39 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (arguments.length === 0 || array.length === 0) {
+        return 0;
+    } else {
+        return Math.min(...array);
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (arguments.length === 0 || array.length === 0) {
+        return 0;
+    } else {
+        return Math.max(...array);
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (arguments.length === 0 || array.length === 0) {
+        return 0;
+    } else {
+        return array.reduce((prev, current) => prev + current, 0) / array.length;
+    }
 }
